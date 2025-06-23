143bf47cca005ab9ba6d06ec0cd68f7cc40fe08d
Rajveer <csb21075@tezu.ac.in>
2025-06-23 08:50:44 +0530

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
+import { Analytics } from '@vercel/analytics/next';
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -13,6 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+       <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
